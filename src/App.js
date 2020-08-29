02ab1f441c009437971f90b48b6ddf4cb4119d38
Abdul Basit <fa17-bce-062@cuilahore.edu.pk>
2020-08-29 13:03:33 +0500

    React Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
 import './App.css';
 
 
@@ -8,17 +9,14 @@ class App extends Component {
     render() {
         return (
 
-            <
-            div className = "App" >
-            <
-            Navbar dark color = "primary" >
-            <
-            div className = "container" >
-            <
-            NavbarBrand href = "/" > Ristorate Con Fusion < /NavbarBrand> < /
-            div > <
-            /Navbar> < /
-            div >
+            <div >
+                <Navbar dark color = "primary" >
+                    <div className = "container" >
+                        <NavbarBrand href = "/" > Ristorante Con Fusion </NavbarBrand>
+                    </div> 
+                </Navbar>
+                <Menu/> 
+            </div>
         );
     }
 }
